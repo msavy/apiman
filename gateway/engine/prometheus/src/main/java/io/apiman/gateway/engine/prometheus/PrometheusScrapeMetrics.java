732974f5eecf328342df8bb3b6642b87bf31feb7
Marc Savy <marc@rhymewithgravy.com>
2015-08-17 12:57:07 +0100

    stuff

--- gateway/engine/prometheus/src/main/java/io/apiman/gateway/engine/prometheus/PrometheusScrapeMetrics.java
+++ gateway/engine/prometheus/src/main/java/io/apiman/gateway/engine/prometheus/PrometheusScrapeMetrics.java
@@ -1,5 +1,27 @@
 package io.apiman.gateway.engine.prometheus;
 
-public class PrometheusScrapeMetrics {
+import java.util.Map;
 
+import io.apiman.gateway.engine.IComponentRegistry;
+import io.apiman.gateway.engine.IMetrics;
+import io.apiman.gateway.engine.metrics.RequestMetric;
+
+public class PrometheusScrapeMetrics implements IMetrics {
+
+    private Map<String, String> config;
+    private IComponentRegistry registry;
+
+    public PrometheusScrapeMetrics(Map<String, String> config) {
+        this.config = config;
+    }
+
+    @Override
+    public void setComponentRegistry(IComponentRegistry registry) {
+        this.registry = registry;
+    }
+
+    @Override
+    public void record(RequestMetric metric) {
+
+    }
 }
