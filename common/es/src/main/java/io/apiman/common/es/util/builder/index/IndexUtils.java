6fe8ccccce1c6fa115da8b280add1631142d436b
Marc Savy <marc@rhymewithgravy.com>
2021-03-02 15:26:25 +0000

    Wire up manager ES indexes

--- common/es/src/main/java/io/apiman/common/es/util/builder/index/IndexUtils.java
+++ common/es/src/main/java/io/apiman/common/es/util/builder/index/IndexUtils.java
@@ -5,6 +5,8 @@ public class IndexUtils {
     public static final EsIndexProperty DATE_PROP = EsIndexProperty.builder().setType("date").build();
     public static final EsIndexProperty IP_PROP = EsIndexProperty.builder().setType("ip").build();
     public static final EsIndexProperty BOOL_PROP = EsIndexProperty.builder().setType("boolean").build();
+    public static final EsIndexProperty BIN_PROP = EsIndexProperty.builder().setType("binary").build();
+    public static final EsIndexProperty OBJECT_PROP = EsIndexProperty.builder().setType("object").build();
     public static final EsField KEYWORD_PROP = KeywordEntryEs.builder().build();
     public static final EsIndexProperty TEXT_AND_KEYWORD_PROP_256 =
         EsIndexProperty.builder().setType("text")
